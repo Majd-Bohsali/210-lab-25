dd9235072f12fe92a98d73b8ed838a3929174d28
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-10-25 15:39:36 -0700

    added count for reading into a vector

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
 // COMSC210 | lab 25 | Majd Bohsali
 // IDE useD: Visual Studio Code
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 #include <chrono>
 #include <vector>
 #include <list>
 #include <set> 
 using namespace std;
+using namespace std::chrono;
 
 int main() {
+    auto start = high_resolution_clock::now();
+    ifstream fin("codes.txt");
+    string code; 
+    // collects file data with vector>
     
+    vector<string> codesVector;
+    while (fin >> code) {
+        codesVector.push_back(code); 
+    }
+    fin.close();
 
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+    cout << "duartion: " << duration.count(); 
+    
     return 0;
 }
 
